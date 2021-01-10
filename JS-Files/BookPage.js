4ea07b8e983418d470d3b8176fe55db9d8ad6182
MiroslavDionisiev <73421736+MiroslavDionisiev@users.noreply.github.com>
2021-01-10 14:50:51 +0200

    added add to cart functionality

--- JS-Files/BookPage.js
+++ JS-Files/BookPage.js
@@ -1,17 +1,45 @@
+var bookInfo = document.getElementsByTagName('body')[0].id;
+var button = document.querySelector("#ToCart");
+var genre = bookInfo.slice(0, bookInfo.length-1);
+var booksInCart = [];
 
-var bookInfoFromPage = JSON.parse(localStorage.getItem("bookInformation")); 
-
-
-function renderBook(){
+db.collection("Books").doc(genre).collection("Titles").doc(bookInfo).get().then(snapshot=>
+{
+    document.title = snapshot.data().name;
     
-    document.title = bookInfoFromPage.data.name;
-    
-    document.getElementById("NameBook").innerHTML = bookInfoFromPage.data.name;
-    document.getElementById("Author").innerHTML = bookInfoFromPage.data.author;
-    document.getElementById("Price").innerHTML = bookInfoFromPage.data.priceStr + " лв.";
-    document.getElementById("ImageBook").src = bookInfoFromPage.img;
+    document.getElementById("NameBook").textContent = snapshot.data().name;
+    document.getElementById("Author").textContent ="Автор: " + snapshot.data().author;
+    document.getElementById("Price").textContent = snapshot.data().priceStr + " лв.";
 
-    document.getElementById("Resume").innerHTML = bookInfoFromPage.data.resume;
+    let storage = firebase.storage();
+    let storageRef = storage.ref();
+    let genreRef = storageRef.child(genre);
+    imageName = bookInfo+".jpg";
+    let imgRef = genreRef.child(imageName);
+
+    imgRef.getDownloadURL().then(function(url) {
+        document.getElementById("ImageBook").setAttribute("src", url);
+    });
+
+    document.getElementById("Resume").textContent = snapshot.data().resume;
+})
+
+function refreshCart()
+{
+    if(JSON.parse(localStorage.getItem("booksInCart"))===null)
+    {
+        localStorage.setItem("booksInCart", booksInCart);
+    }
+    else
+    {
+        let arrTemp = JSON.parse(localStorage.getItem("booksInCart"));
+        arrTemp = arrTemp.concat(booksInCart);
+        localStorage.setItem("booksInCart", JSON.stringify(arrTemp));
+    }
 }
 
-renderBook();
+button.addEventListener("click", ()=>{
+    booksInCart.push(bookInfo);
+    refreshCart();
+    window.location.href = "./Cart.html";
+});
